428819b164902b151bff7331a69b1cd6bff04463
parkgyeongho <kayheypark>
2020-09-11 00:02:47 +0900

    start

---
+++ src/components/Counter.jsx
@@ -0,0 +1,31 @@
+﻿import React, { Component } from 'react';
+
+class Counter extends Component {
+  state = {
+    number: 0,
+  };
+
+  handlePlus = () => {
+    this.setState({
+      number: this.state.number + 1,
+    });
+  };
+  handleMinus = () => {
+    this.setState({
+      number: this.state.number - 1,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>카운터</h1>
+        <div>값 : {this.state.number}</div>
+        <button onClick={this.handlePlus}>+</button>
+        <button onClick={this.handleMinus}>-</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
